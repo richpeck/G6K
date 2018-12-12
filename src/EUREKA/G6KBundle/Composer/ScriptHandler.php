29ce8575b9285185890e7c542b8ed4bd72d4935d
Richard Peck <rpeck@frontlineutilities.co.uk>
2018-12-12 19:45:36 +0000

    Test

--- src/EUREKA/G6KBundle/Composer/ScriptHandler.php
+++ src/EUREKA/G6KBundle/Composer/ScriptHandler.php
@@ -194,29 +194,7 @@ class ScriptHandler
 		if (($parameters = self::getParameters($event, $configDir)) === false) {
 			return;
 		}
-		$name = 'fos_demo';
-		$schemafile = $databasesDir . DIRECTORY_SEPARATOR . $name . '.schema.json';
-		$datafile = $databasesDir . DIRECTORY_SEPARATOR . $name . '.json';
-		$datasources = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><DataSources xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../doc/DataSources.xsd"><Databases></Databases></DataSources>', LIBXML_NOWARNING);
-		$helper = new DatasourcesHelper($datasources);
-		$dsid = 0;
-		$dom = $helper->makeDatasourceDom($schemafile, $datafile, $parameters, $databasesDir, $dsid);
-		$xml = $dom->saveXML(null, LIBXML_NOEMPTYTAG);
-		$dom = new \DOMDocument();
-		$dom->preserveWhiteSpace  = false;
-		$dom->formatOutput = true;
-		$dom->loadXml($xml);
-		$formatted = preg_replace_callback('/^( +)</m', function($a) {
-			return str_repeat("\t", intval(strlen($a[1]) / 2)).'<';
-		}, $dom->saveXML(null, LIBXML_NOEMPTYTAG));
-		file_put_contents($databasesDir."/DataSources.xml", $formatted);
-		$parameters = (object)$parameters;
-		if (file_exists($simusDir . DIRECTORY_SEPARATOR . 'demo-' . $parameters->locale . '.xml')) {
-			rename($simusDir . DIRECTORY_SEPARATOR . 'demo-' . $parameters->locale . '.xml', $simusDir . DIRECTORY_SEPARATOR . 'demo.xml');
-		}
-		foreach (glob($simusDir . DIRECTORY_SEPARATOR . "demo-*.xml") as $filename) {
-			unlink($filename);
-		}
+
 	}
 
 	/**
