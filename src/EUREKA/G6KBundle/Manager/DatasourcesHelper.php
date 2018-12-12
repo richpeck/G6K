4a115a2b104732e7a2d798de11677e8626499082
Richard Peck <rpeck@frontlineutilities.co.uk>
2018-12-12 21:00:23 +0000

    Print

--- src/EUREKA/G6KBundle/Manager/DatasourcesHelper.php
+++ src/EUREKA/G6KBundle/Manager/DatasourcesHelper.php
@@ -72,7 +72,7 @@ class DatasourcesHelper {
 	 *
 	 */
 	public function makeDatasourceDom($schemafile, $datafile, $parameters, $databasesDir, &$id) {
-    echo $parameters;
+    print $parameters;
 		$converter = new JSONToSQLConverter($parameters, $databasesDir);
 		$form = $converter->convert($schemafile, $datafile);
 		$datasource = $this->doCreateDatasource($form);
