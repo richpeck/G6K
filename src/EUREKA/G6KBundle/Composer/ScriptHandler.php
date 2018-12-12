35673cece713dc06dc1b6d2e0ad0e002a2a9385c
Richard Peck <rpeck@frontlineutilities.co.uk>
2018-12-12 20:51:47 +0000

    DBDir

--- src/EUREKA/G6KBundle/Composer/ScriptHandler.php
+++ src/EUREKA/G6KBundle/Composer/ScriptHandler.php
@@ -200,6 +200,9 @@ class ScriptHandler
 		$datasources = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><DataSources xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../doc/DataSources.xsd"><Databases></Databases></DataSources>', LIBXML_NOWARNING);
 		$helper = new DatasourcesHelper($datasources);
 		$dsid = 0;
+		$event->getIO()->write($databaseDir);
+		$dom = $helper->makeDatasourceDom($schemafile, $datafile, $parameters, $databasesDir, $dsid);
+		$xml = $dom->saveXML(null, LIBXML_NOEMPTYTAG);
 
 	}
 
